019d579769f4ed224b6c8ad3b3558ad3a91dca9b
Philip Andreas Kingo <philipandreaskingo@hotmail.com>
2021-10-13 12:07:18 +0200

    added content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: `Anders Kingo`,
-    description: `Foredragsholder & Forfatter. Holder foredrag om Kierkegaard, Leonard Cohen og kristendom.`,
-    siteUrl: `https://anderskingo.dk/`,
+    title: "Anders Kingo",
+    description:
+      "Foredragsholder & Forfatter. Holder foredrag om Kierkegaard, Leonard Cohen og kristendom.",
+    siteUrl: "https://anderskingo.dk/",
     image:
       "https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg",
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-image",
     "gatsby-plugin-postcss",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: "images",
+        path: "${__dirname}/src/images",
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-netlify-cms`,
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-netlify",
   ],
 }
